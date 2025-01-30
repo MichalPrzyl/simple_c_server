ba73e715b42fb50a5689da2e273aec5458723f50
michalPrzyl <michal.przyl@gmail.com>
2025-01-30 20:57:41 +0100

    Add: Error checking

--- main.c
+++ main.c
@@ -21,14 +21,28 @@ int main(){
   char response[] = "Hello from C server!\n";
 
   int s = socket(AF_INET, SOCK_STREAM, 0);
-  bind(s, (struct sockaddr *)&addr, sizeof(addr));
+  if(s < 0){
+    printf("Error while socketing\n");
+  }
+
+  int binded = bind(s, (struct sockaddr *)&addr, sizeof(addr));
+  if(binded < 0){
+    printf("Error while binding\n");
+  }
+
   int result = listen(s, 1);
+  if(result < 0){
+    printf("Error while listening\n");
+  }
 
   socklen_t addr_len = sizeof(addr);
-  int client_fd = accept(s, (struct sockaddr *)&addr, &addr_len);
+  for(;;){
 
-  write(client_fd, response, strlen(response));
-  /* close(client_fd); */
-  /* close(s); */
+    int client_fd = accept(s, (struct sockaddr *)&addr, &addr_len);
+
+    write(client_fd, response, strlen(response));
+    /* close(client_fd); */
+    /* close(s); */
+  }
   return 0;
 }
